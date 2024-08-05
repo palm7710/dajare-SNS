a855777e744bbe46b3be8bc2bec1d2e2a020d13d
palm7710 <241it002i@s.internet.ac.jp>
2024-08-06 02:39:55 +0900

    ロゴを追加しました

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'deep-purple': '#6D4584', // 濃い紫
+          'light-purple': '#AF8CA2', // 薄い紫
+          'custom-gray': '#D9D9D9', // グレー
+          'dialog-gray': 'rgba(199, 199, 199, 0.5)', // ダイアログの背景色
+          'custom-red': '#C62926', // 赤
+          'black': '#000000', // 黒
+          'white': '#ffffff', // 白
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
