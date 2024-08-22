9cc6835219a494b3f9f626291fb206f9a6ddf26d
palm7710 <241it002i@s.internet.ac.jp>
2024-08-22 18:42:23 +0900

    差分

--- database/migrations/2024_08_05_084033_create_dajare_posts_table.php
+++ database/migrations/2024_08_05_084033_create_dajare_posts_table.php
@@ -9,28 +9,28 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    // public function up(): void
-    // {
-    //     Schema::create('dajare_posts', function (Blueprint $table) {
-    //         $table->id()->primary();
-    //         $table->foreignId('user_id')->constrained();
-    //         $table->text('text')->nullable();
-    //         $table->string('image')->nullable();
-    //         $table->integer('dajare_score');
-    //         $table->integer('impression');
-    //         $table->timestamps();
-    //     });
+    public function up(): void
+    {
+        Schema::create('dajare_posts', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->foreignId('user_id')->constrained();
+            $table->text('text')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('dajare_score');
+            $table->integer('impression');
+            $table->timestamps();
+        });
 
-    //     Schema::table('dajare_posts', function (Blueprint $table) {
-    //         DB::statement('ALTER TABLE dajare_posts ADD CONSTRAINT chk_text_or_image CHECK (text IS NOT NULL OR image IS NOT NULL)');
-    //     });
-    // }
+        Schema::table('dajare_posts', function (Blueprint $table) {
+            DB::statement('ALTER TABLE dajare_posts ADD CONSTRAINT chk_text_or_image CHECK (text IS NOT NULL OR image IS NOT NULL)');
+        });
+    }
 
-    // /**
-    //  * Reverse the migrations.
-    //  */
-    // public function down(): void
-    // {
-    //     Schema::dropIfExists('dajare_posts');
-    // }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dajare_posts');
+    }
 };
--- database/migrations/2024_08_05_084150_create_comments_table.php
+++ database/migrations/2024_08_05_084150_create_comments_table.php
@@ -9,38 +9,38 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    // public function up(): void
-    // {
-    //     Schema::create('comments', function (Blueprint $table) {
-    //         $table->id()->primary();
-    //         $table->foreignId('user_id')->constrained();
-    //         $table->foreignId('common_post_id')->nullable()->constrained();
-    //         $table->foreignId('dajare_post_id')->nullable()->constrained();
-    //         $table->string('text')->nullable();
-    //         $table->string('image')->nullable();
-    //         $table->timestamps();
-    //     });
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('common_post_id')->nullable()->constrained();
+            $table->foreignId('dajare_post_id')->nullable()->constrained();
+            $table->string('text')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
 
-    //     Schema::table('comments', function (Blueprint $table) {
-    //         DB::statement('ALTER TABLE comments ADD CONSTRAINT chk_text_or_image CHECK (text IS NOT NULL OR image IS NOT NULL)');
-    //         DB::statement('
-    //             ALTER TABLE comments
-    //             ADD CONSTRAINT chk_common_post_id_or_dajare_post_id_null
-    //             CHECK (
-    //                 (common_post_id IS NULL AND dajare_post_id IS NOT NULL) OR
-    //                 (common_post_id IS NOT NULL AND dajare_post_id IS NULL)
-    //             )
-    //         ');
-    //     });
-    // }
+        Schema::table('comments', function (Blueprint $table) {
+            DB::statement('ALTER TABLE comments ADD CONSTRAINT chk_text_or_image CHECK (text IS NOT NULL OR image IS NOT NULL)');
+            DB::statement('
+                ALTER TABLE comments
+                ADD CONSTRAINT chk_common_post_id_or_dajare_post_id_null
+                CHECK (
+                    (common_post_id IS NULL AND dajare_post_id IS NOT NULL) OR
+                    (common_post_id IS NOT NULL AND dajare_post_id IS NULL)
+                )
+            ');
+        });
+    }
 
 
 
-    // /**
-    //  * Reverse the migrations.
-    //  */
-    // public function down(): void
-    // {
-    //     Schema::dropIfExists('comments');
-    // }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
 };
